d213fc61352c8ea5d62377acdd58e2c59fcb4425
이재용 <dlwodyd62@naver.com>
2024-12-21 13:57:11 +0900

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    dsad
     <h1>Hello</h1>
     <h2>hi</h2>
   </React.StrictMode>
